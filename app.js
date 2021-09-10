e90c806deaa1a0c396aa41ec133a38923116ed12
Haresh Kotkar <75067419+harshkotkar5557@users.noreply.github.com>
2021-09-10 16:21:19 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,37 @@
+const billAmount = document.querySelector("#total_bill");
+const cashGiven = document.querySelector("#cash_given");
+const submit_button =document.getElementById('submit_button')
+const message = document.querySelector("#error-message");
+const noOfNotes = document.querySelectorAll(".no-of-notes");
+
+const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+submit_button.addEventListener("click", function validateBillAndCashAmount() {
+    hideMessage();
+    if (billAmount.value > 0) {
+        if (cashGiven.value >= billAmount.value) {
+            const amountToBeReturned = cashGiven.value - billAmount.value;
+            calculateChange(amountToBeReturned);
+        } else {
+            showMessage(
+                "Cash provided should be equal to the bill amount")
+        }
+    } else {
+        showMessage("Invalid bill amount");
+    }
+});
+function calculateChange(amountToBeReturned) {
+    for (let i = 0; i < availableNotes.length; i++) {
+        const numberOfNotes = Math.trunc(amountToBeReturned / availableNotes[i]);
+        amountToBeReturned = amountToBeReturned % availableNotes[i];
+        noOfNotes[i].innerText = numberOfNotes;
+    }
+}
+
+function hideMessage() {
+    message.style.display = "none";
+}
+
+function showMessage(msg) {
+    message.style.display = "block";
+    message.innerText = msg;
+}
